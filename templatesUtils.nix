d8f6039670b0a0a54c4e75c20635282db4362d7f
Oren Rozen <countoren@gmail.com>
2020-03-28 19:48:06 -0500

    change wrong path for repo add meta data to tarUrlToTemplateDtv

--- templatesUtils.nix
+++ templatesUtils.nix
@@ -1,9 +1,10 @@
 with import <nixpkgs>{};
 rec {
-  tarUrlToDrv = { name, url, meta ? {}}  : repoToDrv { inherit name meta; repo =(fetchTarball url);};
+  tarUrlToDrv = { url, name, meta ? {}}  : repoToDrv { inherit name ; meta = { inherit url;} ; repo =(fetchTarball url);};
 
-  repoToDrv =  repoParams : 
-  if (lib.hasAttr "template.nix" (builtins.readDir ./. ) ) then
+  repoToDrv =  {repo, name, meta ?{} }@repoParams : 
+  assert (lib.assertMsg (lib.isStorePath repo) "repo must be a store path folder");
+  if (lib.hasAttr "template.nix" (builtins.readDir repo) ) then
     import "${repo}/template.nix" repoParams
   else 
     import ./basic-template.nix repoParams;
